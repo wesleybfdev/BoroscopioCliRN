3da7811ddb48eff02ce732f9df690f898aee371c
Wesley - AllRisk <wesley.fernandes@allrisk.com.br>
2025-02-11 09:13:57 -0300

    create boroscopio component and necessary instalations

--- App.tsx
+++ App.tsx
@@ -4,11 +4,16 @@ import {
   Text,
   View,
 } from 'react-native';
+import Boroscopio from './src/components/Boroscopio';
 
 function App() {
   return (
     <View style={styles.container}>
-      <Text>TEST11111</Text>
+      <Boroscopio />
+      {/* 
+        HOWiFi_03e5bb nao funcionou
+        HOWIFI-874d58 testar e fazer rastreio no wireshark
+      */}
     </View>
   );
 }
