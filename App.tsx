baae0b85989aa97ea73e9291153ff950527ed985
Wesley - AllRisk <wesley.fernandes@allrisk.com.br>
2025-02-11 09:10:50 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>TEST11111</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
