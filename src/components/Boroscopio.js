d7e4d2179a62e545500fef9ee7bc46bfd601259f
Wesley - AllRisk <wesley.fernandes@allrisk.com.br>
2025-02-12 10:34:54 -0300

    uma nova forma de gerencias os frames recebidos

--- src/components/Boroscopio.js
+++ src/components/Boroscopio.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Image, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import socket from 'react-native-udp';
 import { Buffer } from 'buffer'
 
@@ -9,7 +9,12 @@ const serverPort = 8030;
 const randomPort = Math.floor(Math.random() * (65535 - 5000 + 1)) + 5000;
 
 const Boroscopio = () => {
-  const [imageData, setImageData] = useState(null);
+  // Estado que armazena a imagem atualmente exibida
+  const [currentImage, setCurrentImage] = useState(null);
+
+  const frameAccumulator = useRef([]); // Array para armazenar todos os frames recebidos via socket em um array unico
+  const framesToCreateImage = useRef([]); // Array para armazenar os frames que serao usados para criar uma "nova image" concatenando os frames nessa var
+  const imageQueue = useRef([]); // Array para armazenar todas as imagens criadas ao concatenar os frames
 
   useEffect(() => {
     const udpClient = socket.createSocket({
@@ -42,48 +47,98 @@ const Boroscopio = () => {
     udpClient.on('message', (msg, rinfo) => {
       if (msg && rinfo) {
         const base64String = msg.toString('base64');
-        processFrames(base64String.slice(32, base64String.length));
+        const frame = base64String.slice(32, base64String.length);
+
+        frameAccumulator.current.push(frame)
+
+        processFrames(frame)
+
+        // console.log({
+        //   frameAccumulator: frameAccumulator.current, 
+        //   framesToCreateImage: framesToCreateImage.current, 
+        //   imageQueue: imageQueue.current
+        // })
       }
     });
 
+    // O intervalo pode ser ajustado de acordo com sua necessidade (por exemplo, 33ms para ~30fps)
+    const interval = setInterval(() => {
+      if (imageQueue.current.length > 0) {
+        // Remove o primeiro item da fila (FIFO) e atualiza a imagem
+        const nextImage = imageQueue.current.shift();
+        setCurrentImage(nextImage);
+      }
+    }, 500);
+
     return () => {
       udpClient.close();
+      clearInterval(interval);
     };
   }, []);
 
-  let frameSize = 0
-  let frameArr = []
+  function processFrames(frame) {
+    if (frame.startsWith('/9j/')) {
+    // if (frame.startsWith('/9j/') && framesToCreateImage.current.length > 0) {
+      const newImage = Buffer.concat(framesToCreateImage.current.map(frame => Buffer.from(frame, 'base64'))).toString('base64');
+      imageQueue.current.push(newImage)
 
-  let framesListArr = []
-  let newImage = []
+      framesToCreateImage.current = []
+    }
 
-  function processFrames(base64String) {
-    if (base64String.includes('/9j/')) {
-
-      const binaryFrames = framesListArr.map(frame => Buffer.from(frame, 'base64'));
-      const mergedBinary = Buffer.concat(binaryFrames);
-      console.log(mergedBinary.toString('base64'))
-      // newImage = `data:image/jpeg;base64,${mergedBinary.toString('base64')}`
-      // console.log(newImage)
-
-      newImage = framesListArr
-
-      framesListArr = []
-    } 
-    
-    framesListArr.push(base64String)
+    framesToCreateImage.current.push(frame)
   }
 
+  // useEffect(() => {
+  //   // O intervalo pode ser ajustado de acordo com sua necessidade (por exemplo, 33ms para ~30fps)
+  //   const interval = setInterval(() => {
+  //     if (imageQueue.current.length > 0) {
+  //       // Remove o primeiro item da fila (FIFO) e atualiza a imagem
+  //       const nextImage = imageQueue.current.shift();
+  //       setCurrentImage(nextImage);
+  //     }
+  //   }, 500);
+
+  //   // Limpa o intervalo ao desmontar o componente
+  //   return () => clearInterval(interval);
+  // }, []);
+
   return (
-    <View style={{ flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }}>
-      {/* {imageData && (
+    <View style={styles.container}>
+      {currentImage ? (
         <Image
-          source={{ uri: imageData }}
-          style={{ width: 300, height: 200 }}
+          style={styles.image}
+          source={{ uri: `data:image/png;base64,${currentImage}` }}
+          resizeMode="contain"
         />
-      )} */}
+      ) : (
+        <View style={{ backgroundColor: 'white'}}>
+          <Text>Carregando ...</Text>
+        </View>
+      )}
+
+      <Button
+        title="Dado atual"
+        onPress={() => console.log(imageQueue.current)}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 16 / 9, 
+  },
+  placeholder: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default Boroscopio;
